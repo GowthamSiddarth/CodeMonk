5fcce8737c2ff321f6b33b5bd571f80c61172cdb
Gowtham Behara <gothsiddu@gmail.com>
2018-12-17 12:18:19 +0530

    Reformat code

--- BasicsOfProgramming/BasicsOfInputOutput/PrimeNumber.c
+++ BasicsOfProgramming/BasicsOfInputOutput/PrimeNumber.c
@@ -1,12 +1,14 @@
 /**
- * 
+ *
 Problem Statement:
 
-You are given an integer N. You need to print the series of all prime numbers till N.
+You are given an integer N. You need to print the series of all prime numbers
+till N.
 
 Input Format
 
-The first and only line of the input contains a single integer N denoting the number till where you need to find the series of prime number.
+The first and only line of the input contains a single integer N denoting the
+number till where you need to find the series of prime number.
 
 Output Format
 
@@ -20,34 +22,34 @@ https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-in
 
 */
 
-#include <stdio.h>
 #include <malloc.h>
 #include <math.h>
+#include <stdio.h>
 
 void sieveOfEratosthenes(int *flag, int n) {
-    int m = sqrt(n);
-    for (int i = 2; i <= m; i++) {
-        if (0 == flag[i]) {
-            for (int j = i * 2; j <= n; j += i) {
-                flag[j] = 1;
-            }
-        }
+  int m = sqrt(n);
+  for (int i = 2; i <= m; i++) {
+    if (0 == flag[i]) {
+      for (int j = i * 2; j <= n; j += i) {
+        flag[j] = 1;
+      }
     }
+  }
 }
 
 void printPrimesInRange(int n) {
-    int *flag = (int *)calloc(n + 1, sizeof(int));
-    
-    sieveOfEratosthenes(flag, n);
-    for (int i = 2; i <= n; i++) {
-        if (0 == flag[i]) {
-            printf("%d ", i);
-        }
+  int *flag = (int *)calloc(n + 1, sizeof(int));
+
+  sieveOfEratosthenes(flag, n);
+  for (int i = 2; i <= n; i++) {
+    if (0 == flag[i]) {
+      printf("%d ", i);
     }
+  }
 }
 
 int main() {
-    int n = 9;
-    printPrimesInRange(n);
-    return 0;
+  int n = 9;
+  printPrimesInRange(n);
+  return 0;
 }
