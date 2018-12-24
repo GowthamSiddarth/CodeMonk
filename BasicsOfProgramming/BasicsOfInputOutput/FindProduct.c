69749268695ca956d66a1fc0799f3336ddd4372c
Gowtham Behara <gothsiddu@gmail.com>
2018-12-24 12:29:15 +0530

    Call findProduct func through main with modulo

--- BasicsOfProgramming/BasicsOfInputOutput/FindProduct.c
+++ BasicsOfProgramming/BasicsOfInputOutput/FindProduct.c
@@ -1,6 +1,7 @@
 /**
 You have been given an array A of size N consisting of positive integers. You
-need to find and print the product of all the number in this array Modulo 10^9 + 7.
+need to find and print the product of all the number in this array Modulo 10^9
++ 7.
 
 Input Format:
 The first line contains a single integer N denoting the size of the array. The
@@ -14,12 +15,24 @@ Constraints:
 1 <= N <= 1000
 1 <= A[i] <= 1000
 */
+#include <stdio.h>
 
 int findProduct(int *arr, int len, int modulo) {
-    int product = 1;
-    for (int i = 0; i < len; i++) {
-        product = (product * arr[i]) % modulo;
-    }
+  int product = 1;
+  for (int i = 0; i < len; i++) {
+    product = (product * arr[i]) % modulo;
+  }
 
-    return product;
+  return product;
+}
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+  int len = sizeof(arr) / sizeof(arr[0]);
+  int modulo = 1000000007;
+
+  int res = findProduct(arr, len, modulo);
+  printf("%d", res);
+
+  return 0;
 }
