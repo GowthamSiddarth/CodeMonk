0576c45061af7842c8bb35c0e319fd34048a90cb
Gowtham Behara <gothsiddu@gmail.com>
2018-12-30 08:36:59 +0530

    Define func to find length of string

--- BasicsOfProgramming/BasicsOfImplementation/PassingTheParcel.c
+++ BasicsOfProgramming/BasicsOfImplementation/PassingTheParcel.c
@@ -41,3 +41,11 @@ Constraints :
 1 <= |S| <= 1000, where |S| denotes the length of the input string. It is
 guaranteed that at least 1 lyric in the song will be a 'b'
 */
+int lengthOfString(char *s) {
+    int idx = 0;
+    while ('\0' != s[idx]) {
+        idx++;
+    }
+
+    return idx;
+}
