9fbb21b79c1022c4d56ce29d2bd0ac3914ac24a6
Gowtham Behara <gothsiddu@gmail.com>
2019-01-18 10:55:14 +0530

    Define func to check if string is complete

--- BasicsOfProgramming/BasicsOfImplementation/CompleteString.c
+++ BasicsOfProgramming/BasicsOfImplementation/CompleteString.c
@@ -16,3 +16,22 @@ characters a to z
 
 https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/complete-string-4/
 */
+#define COMPLETE 1
+#define NOT_COMPLETE 0
+
+int isCompleteString(char *string) {
+    int occurrence[26] = {0};
+    int idx = 0;
+
+    while ('\0' != string[idx]) {
+        occurrence[string[idx] - 'a']++;
+        idx++;
+    }
+
+    idx = 0;
+    while (idx < 26 && occurrence[idx] > 0) {
+        idx++;
+    }
+
+    return 26 == idx ? COMPLETE : NOT_COMPLETE;
+}
