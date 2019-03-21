c8468051aea081f1a8b8f6c3893e09f8628b805f
Gowtham Behara <gothsiddu@gmail.com>
2019-03-21 20:36:00 +0530

    Define struct position

--- BasicsOfProgramming/BasicsOfImplementation/FinalDestination.c
+++ BasicsOfProgramming/BasicsOfImplementation/FinalDestination.c
@@ -21,3 +21,7 @@ Constraints:
 
 https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/final-destination-cakewalk/
 */
+
+struct Position {
+    int x, y;
+};
