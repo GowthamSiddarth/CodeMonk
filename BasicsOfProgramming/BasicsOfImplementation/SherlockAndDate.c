27c8f8b8589d77755de2ea717f46ee50df98ecb6
Gowtham Behara <gothsiddu@gmail.com>
2019-03-24 08:35:37 +0530

    Add problem statement

---
+++ BasicsOfProgramming/BasicsOfImplementation/SherlockAndDate.c
@@ -0,0 +1,16 @@
+/**
+Watson gives a date to Sherlock and asks him what is the date on the previous
+day. Help Sherlock. You are given date in DD MM YYYY format. DD is an integer
+without leading zeroes. MM is one of the "January", "February", "March",
+"April", "May", "June", "July", "August", "September", "October", "November" and
+"December" (all quotes for clarity). YYYY is also an integer between 1600 and
+2000 without leading zeroes. You have to print the date of the previous day in
+same format.
+
+Input and Output
+First line, T (≤ 100), the number of testcases. Each testcase consists of date
+in specified format. For each testcase, print the required answer in one line.
+Given date will be a valid date.
+
+https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/sherlock-and-date/
+*/
