e89282f1ba5d5529b37952df0588f92f690a9474
Gowtham Behara <gothsiddu@gmail.com>
2019-03-24 11:23:27 +0530

    Define arr inside getTimeTakenByRobot func; check if curr char button not eq to prev button

--- BasicsOfProgramming/BasicsOfImplementation/RoyAndTextingRobot.c
+++ BasicsOfProgramming/BasicsOfImplementation/RoyAndTextingRobot.c
@@ -47,140 +47,142 @@ Note: We are using "_" underscore instead of " " empty space to avoid confusion
 
 https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/roy-and-texting-robot-2/
 */
-
-int buttons[256];
-int positions[256];
-
-buttons['a'] = 2;
-positions['a'] = 1;
-
-buttons['a'] = 2;
-positions['a'] = 1;
-
-buttons['b'] = 2;
-positions['b'] = 2;
-
-buttons['c'] = 2;
-positions['c'] = 3;
-
-buttons['d'] = 3;
-positions['d'] = 1;
-
-buttons['e'] = 3;
-positions['e'] = 2;
-
-buttons['f'] = 3;
-positions['f'] = 3;
-
-buttons['g'] = 4;
-positions['g'] = 1;
-
-buttons['h'] = 4;
-positions['h'] = 2;
-
-buttons['i'] = 4;
-positions['i'] = 3;
-
-buttons['j'] = 5;
-positions['j'] = 1;
-
-buttons['k'] = 5;
-positions['k'] = 2;
-
-buttons['l'] = 5;
-positions['l'] = 3;
-
-buttons['m'] = 6;
-positions['m'] = 1;
-
-buttons['n'] = 6;
-positions['n'] = 2;
-
-buttons['o'] = 6;
-positions['o'] = 3;
-
-buttons['p'] = 7;
-positions['p'] = 1;
-
-buttons['q'] = 7;
-positions['q'] = 2;
-
-buttons['r'] = 7;
-positions['r'] = 3;
-
-buttons['s'] = 7;
-positions['s'] = 4;
-
-buttons['t'] = 8;
-positions['t'] = 1;
-
-buttons['u'] = 8;
-positions['u'] = 2;
-
-buttons['v'] = 8;
-positions['v'] = 3;
-
-buttons['w'] = 9;
-positions['w'] = 1;
-
-buttons['x'] = 9;
-positions['x'] = 2;
-
-buttons['y'] = 9;
-positions['y'] = 3;
-
-buttons['z'] = 9;
-positions['z'] = 4;
-
-buttons['.'] = 1;
-positions['.'] = 1;
-
-buttons[','] = 1;
-positions[','] = 2;
-
-buttons['?'] = 1;
-positions['?'] = 3;
-
-buttons['!'] = 1;
-positions['!'] = 4;
-
-buttons['_'] = 0;
-positions['_'] = 1;
-
-buttons['0'] = 0;
-positions['0'] = 2;
-
-buttons['1'] = 1;
-positions['1'] = 5;
-
-buttons['2'] = 2;
-positions['2'] = 4;
-
-buttons['3'] = 3;
-positions['3'] = 4;
-
-buttons['4'] = 4;
-positions['4'] = 4;
-
-buttons['5'] = 5;
-positions['5'] = 4;
-
-buttons['6'] = 6;
-positions['6'] = 4;
-
-buttons['7'] = 7;
-positions['7'] = 5;
-
-buttons['8'] = 8;
-positions['8'] = 4;
-
-buttons['9'] = 9;
-positions['9'] = 5;
+#include <stdio.h>
+#include <string.h>
 
 int getTimeTakenByRobot(char *text) {
+    int buttons[256];
+    int positions[256];
+
+    buttons['a'] = 2;
+    positions['a'] = 1;
+
+    buttons['a'] = 2;
+    positions['a'] = 1;
+
+    buttons['b'] = 2;
+    positions['b'] = 2;
+
+    buttons['c'] = 2;
+    positions['c'] = 3;
+
+    buttons['d'] = 3;
+    positions['d'] = 1;
+
+    buttons['e'] = 3;
+    positions['e'] = 2;
+
+    buttons['f'] = 3;
+    positions['f'] = 3;
+
+    buttons['g'] = 4;
+    positions['g'] = 1;
+
+    buttons['h'] = 4;
+    positions['h'] = 2;
+
+    buttons['i'] = 4;
+    positions['i'] = 3;
+
+    buttons['j'] = 5;
+    positions['j'] = 1;
+
+    buttons['k'] = 5;
+    positions['k'] = 2;
+
+    buttons['l'] = 5;
+    positions['l'] = 3;
+
+    buttons['m'] = 6;
+    positions['m'] = 1;
+
+    buttons['n'] = 6;
+    positions['n'] = 2;
+
+    buttons['o'] = 6;
+    positions['o'] = 3;
+
+    buttons['p'] = 7;
+    positions['p'] = 1;
+
+    buttons['q'] = 7;
+    positions['q'] = 2;
+
+    buttons['r'] = 7;
+    positions['r'] = 3;
+
+    buttons['s'] = 7;
+    positions['s'] = 4;
+
+    buttons['t'] = 8;
+    positions['t'] = 1;
+
+    buttons['u'] = 8;
+    positions['u'] = 2;
+
+    buttons['v'] = 8;
+    positions['v'] = 3;
+
+    buttons['w'] = 9;
+    positions['w'] = 1;
+
+    buttons['x'] = 9;
+    positions['x'] = 2;
+
+    buttons['y'] = 9;
+    positions['y'] = 3;
+
+    buttons['z'] = 9;
+    positions['z'] = 4;
+
+    buttons['.'] = 1;
+    positions['.'] = 1;
+
+    buttons[','] = 1;
+    positions[','] = 2;
+
+    buttons['?'] = 1;
+    positions['?'] = 3;
+
+    buttons['!'] = 1;
+    positions['!'] = 4;
+
+    buttons['_'] = 0;
+    positions['_'] = 1;
+
+    buttons['0'] = 0;
+    positions['0'] = 2;
+
+    buttons['1'] = 1;
+    positions['1'] = 5;
+
+    buttons['2'] = 2;
+    positions['2'] = 4;
+
+    buttons['3'] = 3;
+    positions['3'] = 4;
+
+    buttons['4'] = 4;
+    positions['4'] = 4;
+
+    buttons['5'] = 5;
+    positions['5'] = 4;
+
+    buttons['6'] = 6;
+    positions['6'] = 4;
+
+    buttons['7'] = 7;
+    positions['7'] = 5;
+
+    buttons['8'] = 8;
+    positions['8'] = 4;
+
+    buttons['9'] = 9;
+    positions['9'] = 5;
+
     int idx = 0, prevButton = 1, count = 0;
     while ('\0' != text[idx]) {
-        if (prevButton == buttons[text[idx]]) {
+        if (prevButton != buttons[text[idx]]) {
             count++;
             prevButton = buttons[text[idx]];
         }
@@ -190,4 +192,14 @@ int getTimeTakenByRobot(char *text) {
     }
 
     return count;
+}
+
+int main() {
+    char text[1001];
+    memcpy(text, "i_?_u", 6);
+
+    int res = getTimeTakenByRobot(text);
+    printf("%d\n", res);
+
+    return 0;
 }
